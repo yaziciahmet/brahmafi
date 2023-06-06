bfcd05f4905f3021895a23db72dfe7e8db74484b
yaziciahmet <yaziciahmet991@gmail.com>
2023-06-06 23:07:39 +0300

    add fk and index to migrations

--- migrations/001_init_schema.up.sql
+++ migrations/001_init_schema.up.sql
@@ -11,5 +11,11 @@ CREATE TABLE pool_snapshot(
     token0_balance int not null,
     token1_balance int not null,
     tick int not null,
-    block_number int not null
+    block_number int not null,
+    taken_at timestamptz default now() not null
 );
+
+ALTER TABLE pool_snapshot ADD CONSTRAINT pool_fk
+FOREIGN KEY (pool_id) REFERENCES pool(id);
+
+CREATE INDEX pool_snapshot_pool_idx ON pool_snapshot(pool_id);
